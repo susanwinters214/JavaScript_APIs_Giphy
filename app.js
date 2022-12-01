0ce7513185fdd56e9f0d88e1216d3181be196dae
Susan Winters <susanwinters214@gmail.com>
2022-12-01 10:24:34 -0600

    added new code

--- app.js
+++ app.js
@@ -1,4 +1,24 @@
-console.log("Hello World!\n==========\n");
 
-// Exercise 1 Section
-console.log("EXERCISE 1:\n==========\n");
+
+let searchBtn = document.getElementById("submitSearch");
+let searchInput = document.getElementById("searchWord");
+let feedbackEle = document.getElementById("feedback");
+let img = document.querySelector("img");
+
+searchBtn.addEventListener("click", () => {
+  fetch(
+    "https://api.giphy.com/v1/gifs/translate?api_key=EyJXJeOoltkXKW2L1gJbcHY4uq2zul65&s=" +
+      searchInput.value,
+    { mode: "cors" }
+  )
+    .then((response) => response.json())
+    .then((res) => {
+      feedbackEle.textContent = "";
+      img.src = res.data.images.original.url;
+      searchInput.value = "";
+    })
+    .catch((err) => {
+      console.error(err);
+      feedbackEle.textContent = err.message;
+    });
+});
